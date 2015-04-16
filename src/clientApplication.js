37011ce08971d6d62170514cbe797690a423073d
Björn Breitgoff <syntheticore@gmail.com>
2015-04-17 01:02:13 +0200

    Allow delivering bundle asynchronously in header

--- src/clientApplication.js
+++ src/clientApplication.js
@@ -29,8 +29,10 @@ var ClientApplication = function() {
       var evaluator = Evaluator(body, viewModels, templates, DOMInterface());
       evaluator.baseScope.addLayer(mainModel);
       var frag = evaluator.render(function() {
-        $('body').replaceWith(frag);
-        cb();
+        $(document).ready(function() {
+          $('body').replaceWith(frag);
+          cb();
+        })
       });
     });
   };
