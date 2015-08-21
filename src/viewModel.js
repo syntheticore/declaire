9d974aaedd3188cf1ec6d6bad35f8ad7009d9403
Björn Breitgoff <syntheticore@gmail.com>
2015-08-21 17:05:49 +0200

    Emit attach event for view models

--- src/viewModel.js
+++ src/viewModel.js
@@ -6,7 +6,7 @@ var Model = require('./model.js');
 // template scopes with dynamic data
 // The constructor function may return a promise if it needs
 // to do aynchronous work to set up model instances
-var ViewModel = function(name, reference, constructor, postCb) {
+var ViewModel = function(name, reference, constructor) {
   var model = Model('_view', reference);
   return {
     klass: 'ViewModel',
@@ -14,16 +14,16 @@ var ViewModel = function(name, reference, constructor, postCb) {
 
     // Return a fully resolved and constructed view model instance
     create: function(args, elem, cb) {
-      var self = this;
       var inst = model.create();
       inst.el = elem;
       inst.resolve(function() {
         var post = function() {
           cb(inst);
-          if(postCb && _.onClient()) {
+          if(_.onClient()) {
             _.defer(function() {
               // self.app.onAttach(function() {
-                postCb.apply(inst);
+                // postCb.apply(inst);
+                inst.emit('attach');
               // });
             });
           }
