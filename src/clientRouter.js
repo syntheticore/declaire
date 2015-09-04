3183108e25ba5d31ee233577dfc6fe1489b52748
Björn Breitgoff <syntheticore@gmail.com>
2015-09-04 22:06:01 +0200

    Fix scrolling to anchors from hijacked links

--- src/clientRouter.js
+++ src/clientRouter.js
@@ -4,33 +4,58 @@ var _ = require('./utils.js');
 
 var Router = function() {
   var listeners = {};
+  var suppressNextPopstate = false;
+
+  var scrollTo = function(id) {
+    document.body.scrollTop = document.getElementById(id).offsetTop;
+  };
 
   var self = {
     on: function(path, cb) {
       listeners[path] = cb;
     },
 
+    // Push url to the stack and and trigger popstate event
     navigate: function(url) {
       history.pushState({}, '', url);
       window.onpopstate();
+      // Try to scroll to anchor after page has changed
+      var id = window.location.hash.slice(1);
+      _.defer(function() {
+        if(id) scrollTo(id);
+      }, 200);
     },
 
+    // Prevent page internal links from reloading the page
     hijackLocalLinks: function() {
       var self = this;
       $('body').on('click', 'a', function(e) {
         var href = $(this).attr('href');
+        // Internal links
         if(href.slice(0, 1) == '/' && href.slice(1, 2) != '/') {
           e.preventDefault();
-          self.navigate(href);
+          // Navigate locally unless target url is the same
+          if(window.location.pathname + window.location.hash != href) self.navigate(href);
           return false;
+        // Simple anchors
+        } else if(href.slice(0, 1) == '#') {
+          // Use browsers default behaviour of scrolling
+          // to referenced ID, but keep the view from updating otherwise
+          suppressNextPopstate = true;
         }
+        // Full load of external link otherwise
       });
     }
   };
 
   window.onpopstate = function(event) {
     // var state = event.state;
+    if(suppressNextPopstate) {
+      suppressNextPopstate = false;
+      return;
+    }
     var url = document.location.pathname;
+    // Extract url params and call listeners
     _.each(listeners, function(listener, path) {
       var params = _.values(_.extractUrlParams(url, path));
       if(params || path == '*') {
@@ -38,8 +63,6 @@ var Router = function() {
       }
     });
   };
-
-  // _.defer(window.onpopstate);
 
   return self;
 };
